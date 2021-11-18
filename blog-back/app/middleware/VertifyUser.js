bd4ab8082ee7ff163ab701e26a1062b5af725889
yanyinuo <1601530253@qq.com>
2021-11-18 15:26:27 +0800

    21-11-18

--- blog-back/app/middleware/VertifyUser.js
+++ blog-back/app/middleware/VertifyUser.js
@@ -2,30 +2,26 @@
 *   用于在插入数据库前检查 用户的 账号和密码是否符合条件
 */
 const errType = require("../../constants/errorType")
-// const service = require("../service/user.service")
-const MD5password = require("../../utils/MD5")
-
-
+const errorHandle = require("../../constants/errorHandle")
 module.exports = (options, app) => {
     //判断注册是否符合规范
     return async function vertifyUser(ctx, next) {
-        //1.获取 user，password
         const { username, password } = ctx.request.body;
-        console.log(username, password);
-        //2.判断 是否为空
+        //判断 是否为空
         if (!username || !password) {
-            const error = new Error(errType.NAME_OR_PASSWORD_IS_REQUIRED)
-            console.log(error);
-            ctx.body = error
-            // return ctx.app.emit('error', error, ctx)
+            const error = errType.NAME_OR_PASSWORD_IS_REQUIRED
+            const { status, message } = errorHandle(error)
+            ctx.status = status
+            ctx.body = message
+
         }
         //判断是否有相同的名字，有就无法注册
         const result = await service.getUserByName(username)
         if (result.length) {
-            const error = new Error(errType.USER_ALREADY_EXISTS)
-            console.log(error);
-            ctx.body = error
-            // return ctx.app.emit('error', error, ctx)
+            const error = errType.USER_ALREADY_EXISTS
+            const { status, message } = errorHandle(error)
+            ctx.status = status
+            ctx.body = message
         }
         await next()
     }
