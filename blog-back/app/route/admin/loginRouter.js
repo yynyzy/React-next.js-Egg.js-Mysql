182b2a53c4d0b5583dfb6646b0cba81188905029
yanyinuo <1601530253@qq.com>
2021-11-18 15:25:39 +0800

    21-11-18

--- blog-back/app/route/admin/loginRouter.js
+++ blog-back/app/route/admin/loginRouter.js
@@ -1,7 +1,8 @@
 module.exports = (app) => {
     const { router, controller } = app;
-    const vertifyUser = app.middleware.vertifyUser();
-    const passwordHandle = app.middleware.passwordHandle();
+    const vertifyUser = app.middleware.vertifyUser(); //注册功能的中间件（检查）
+    const passwordHandle = app.middleware.passwordHandle();//注册功能的中间件（MD5加密）
+    const vertifyAuth = app.middleware.vertifyAuth();//登录功能的中间件（检查）
     router.post('/admin/register', vertifyUser, passwordHandle, controller.admin.login.createUser)
-    router.post('/admin/login', controller.admin.login.login)
+    router.post('/admin/login', vertifyAuth, controller.admin.login.login)
 }
